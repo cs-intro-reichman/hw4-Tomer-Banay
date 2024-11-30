208b72f72778f5efbc93388f2a9ded520954612d
Tomer-Banay <tomer.banay@post.runi.ac.il>
2024-11-30 19:13:34 +0200

    3/4

--- MyString.java
+++ MyString.java
@@ -18,13 +18,42 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
-    }
+        // new array for string
+        char [] arr = new char [str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr [i] = str.charAt(i);
+        }
+        // ASCII algorithm
+        for (int i = 0; i < str.length(); i++) {
+            if ('A' <= arr[i] && arr[i] <= 'Z') {
+                arr[i] = (char)(arr[i] + 32);
+            }
+
+        }
+        return new String(arr);
+}
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+
+        if (str1.length() < str2.length())
+        return false;
+
+      
+        for (int i = 0 ; i <str1.length() - str2.length() ; i++) {
+            int j = 0;
+
+            // check start index
+            while (j < str2.length() && str1.charAt(i + j) == str2.charAt(j)) {
+                j++;
+            }
+
+            // If we matched
+            if (j == str2.length()) {
+                return true;
+            }
+        }
         return false;
     }
 }
+
